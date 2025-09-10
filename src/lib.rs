deec6fdff74171bcc714eccf116ba8ff534e4901
Jonathan Hahne <jonathanhahne@n139-p168.eduroam.kth.se>
2025-09-10 09:11:35 +0200

    redefined variables with better names

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ pub mod movement;
 pub mod pieces;
 use std::io;
 pub fn input() -> (usize, usize) {
-    let mut f_row = String::new();
-    let mut f_col = String::new();
-    io::stdin().read_line(&mut f_row).unwrap();
-    let f_row: usize = f_row.trim().parse().unwrap();
-    io::stdin().read_line(&mut f_col).unwrap();
-    let f_col: usize = f_col.trim().parse().unwrap();
+    let mut x = String::new();
+    let mut y = String::new();
+    io::stdin().read_line(&mut x).unwrap();
+    let x: usize = x.trim().parse().unwrap();
+    io::stdin().read_line(&mut y).unwrap();
+    let y: usize = y.trim().parse().unwrap();
 
-    return (f_row, f_col);
+    return (x, y);
 }
