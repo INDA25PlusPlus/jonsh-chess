4a6430e494b4b72e6953e7ca0c758fa8fcc423e4
Jonathan Hahne <jonathanhahne@n139-p168.eduroam.kth.se>
2025-09-10 09:07:48 +0200

    Defined function for input to get x and y coordinates

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 pub mod board;
 pub mod movement;
 pub mod pieces;
+use std::io;
+pub fn input() -> (usize, usize) {
+    let mut f_row = String::new();
+    let mut f_col = String::new();
+    io::stdin().read_line(&mut f_row).unwrap();
+    let f_row: usize = f_row.trim().parse().unwrap();
+    io::stdin().read_line(&mut f_col).unwrap();
+    let f_col: usize = f_col.trim().parse().unwrap();
+
+    return (f_row, f_col);
+}
