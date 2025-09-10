19609cdb28bef627a2d0ac6faad8fcfc5d1e42aa
Jonathan Hahne <jonathanhahne@n139-p168.eduroam.kth.se>
2025-09-10 16:19:39 +0200

    Discovered that I may have switched places of the numerical and alphabetical sides of the chess board, try to fix

--- src/movement.rs
+++ src/movement.rs
@@ -1,14 +1,58 @@
 use crate::board::{Board, Tile};
 use crate::pieces::{Color, Piece};
-use std::io;
+use std::io::{self, Empty};
 
 impl Board {
-    pub fn move_piece(mut self, from_x: usize, from_y: usize, to_x: usize, to_y: usize) -> Self {
+    pub fn move_piece_basic(
+        mut self,
+        from_x: usize,
+        from_y: usize,
+        to_x: usize,
+        to_y: usize,
+    ) -> Self {
         // rudimentary movement function 🥳
         let curr_piece = self.tiles[from_x][from_y]; // get selected piece info
         self.tiles[to_x][to_y] = curr_piece; // move piece to new place
         self.tiles[from_x][from_y] = Tile::Empty; // delete last remaining instance of the piece
         return self; //BOOOOOOOOOOOOOOMMMM, EZ win, WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎
+    }
+    pub fn move_piece(mut self, from_x: usize, from_y: usize, to_x: usize, to_y: usize) -> Self {
+        let selected_piece = self.tiles[from_x][from_y];
+        if let Tile::Occupied(_, Piece::Pawn) = selected_piece {
+            if from_x == to_x {
+                if let Tile::Occupied(Color::Black, _) = selected_piece {
+                    if from_y == 1 {
+                        if to_y - from_y == 1 || to_y - from_y == 2 {
+                            self.tiles[to_x][to_y] = selected_piece;
+                            self.tiles[from_x][from_y] = Tile::Empty;
+                        }
+                    } else {
+                        if to_y - from_y == 1 {
+                            self.tiles[to_x][to_y] = selected_piece;
+                            self.tiles[from_x][from_y] = Tile::Empty;
+                        }
+                    }
+                } else {
+                    if from_y == 6 {
+                        if from_y - to_y == 1 || from_y - to_y == 2 {
+                            self.tiles[to_x][to_y] = selected_piece;
+                            self.tiles[from_x][from_y] = Tile::Empty;
+                        }
+                    } else {
+                        if from_y - to_y == 1 {
+                            self.tiles[to_x][to_y] = selected_piece;
+                            self.tiles[from_x][from_y] = Tile::Empty;
+                        }
+                    }
+                }
+            }
+        } else if let Tile::Empty = selected_piece {
+            unimplemented!("Fuck you");
+        } else {
+            self.tiles[to_x][to_y] = selected_piece;
+            self.tiles[from_x][from_y] = Tile::Empty;
+        }
+        return self;
     }
 }
 
@@ -19,23 +63,31 @@ mod tests {
     use super::*;
     #[test]
     fn print_piece() {
-        let mut board = Board::new();
+        let board = Board::new();
         let (from_x, from_y) = input();
         if let Tile::Occupied(_, piece) = board.tiles[from_x][from_y] {
             println!("{:?}", piece);
         }
     }
     #[test]
-    fn test_move_piece() {
-        let mut board = Board::new();
+    fn test_move_piece_basic() {
+        let board = Board::new();
         let (from_x, from_y) = input();
         let (to_x, to_y) = input();
-        let board = board.move_piece(from_x, from_y, to_x, to_y);
+        let board = board.move_piece_basic(from_x, from_y, to_x, to_y);
         Board::print_board(board);
     }
     #[test]
     fn test_input() {
         let (x, y) = input();
         println!("{:?}", (x, y));
+    }
+    #[test]
+    fn test_move_piece() {
+        let board = Board::new();
+        let (from_x, from_y) = input();
+        let (to_x, to_y) = input();
+        let board = board.move_piece(from_x, from_y, to_x, to_y);
+        Board::print_board(board);
     }
 }
