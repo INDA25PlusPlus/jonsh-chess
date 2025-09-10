01d8c4efc619c93855079cee530c138848603163
Jonathan Hahne <jonathanhahne@n139-p168.eduroam.kth.se>
2025-09-10 09:42:11 +0200

    Added comments and implemented functionality to write coordinates in ches notation

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ pub mod movement;
 pub mod pieces;
 use std::io;
 pub fn input() -> (usize, usize) {
-    let mut x = String::new();
-    let mut y = String::new();
-    io::stdin().read_line(&mut x).unwrap();
-    let x: usize = x.trim().parse().unwrap();
-    io::stdin().read_line(&mut y).unwrap();
-    let y: usize = y.trim().parse().unwrap();
-
-    return (x, y);
+    let mut x = String::new(); //create x variable
+    let mut y = String::new(); //create y variable
+    io::stdin().read_line(&mut x).unwrap(); //read input from terminal
+    let x: char = x.trim().parse().unwrap(); //convert input to char i.e: input is "a\n" or "a\r\n" and a is output
+    let x: u32 = x.to_digit(18).unwrap() - 10; //convert char to int
+    io::stdin().read_line(&mut y).unwrap(); //read input from terminal
+    let y: usize = y.trim().parse().unwrap(); //convert input to int
+    return (x as usize, y); //return both values as a tuple
 }
