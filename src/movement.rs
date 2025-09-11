40b65edc2235483be5838dfc69c7db9a7e864e2a
Jonathan Hahne <jonathanhahne@n129-p143.eduroam.kth.se>
2025-09-11 18:16:10 +0200

    Implemented functionality to move each piece according to the pieces specific rules, though instances where pieces are placed on top of each other is not yet functional

--- src/movement.rs
+++ src/movement.rs
@@ -1,6 +1,5 @@
 use crate::board::{Board, Tile};
 use crate::pieces::{Color, Piece};
-use std::io::{self, Empty};
 
 impl Board {
     pub fn move_piece_basic(
@@ -16,38 +15,87 @@ impl Board {
         self.tiles[from_y][from_x] = Tile::Empty; // delete last remaining instance of the piece
         return self; //BOOOOOOOOOOOOOOMMMM, EZ win, WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎
     }
+
     pub fn move_piece(mut self, from_x: usize, from_y: usize, to_x: usize, to_y: usize) -> Self {
         let selected_piece = self.tiles[from_y][from_x];
-        if let Tile::Occupied(_, Piece::Pawn) = selected_piece {
-            if from_x == to_x {
-                if let Tile::Occupied(Color::Black, _) = selected_piece {
-                    if from_y == 1 {
-                        if to_y - from_y == 1 || to_y - from_y == 2 {
-                            self.tiles[to_y][to_x] = selected_piece;
-                            self.tiles[from_y][from_x] = Tile::Empty;
-                        }
-                    } else {
-                        if to_y - from_y == 1 {
-                            self.tiles[to_y][to_x] = selected_piece;
-                            self.tiles[from_y][from_x] = Tile::Empty;
-                        }
+
+        if let Tile::Occupied(_, Piece::Pawn) = selected_piece // rudimentary pawn movement
+            && from_x == to_x
+        {
+            if let Tile::Occupied(Color::Black, _) = selected_piece {
+                if from_y == 1 {
+                    if to_y - from_y == 1 || to_y - from_y == 2 {
+                        self.tiles[to_y][to_x] = selected_piece;
+                        self.tiles[from_y][from_x] = Tile::Empty;
                     }
                 } else {
-                    if from_y == 6 {
-                        if from_y - to_y == 1 || from_y - to_y == 2 {
-                            self.tiles[to_y][to_x] = selected_piece;
-                            self.tiles[from_y][from_x] = Tile::Empty;
-                        }
-                    } else {
-                        if from_y - to_y == 1 {
-                            self.tiles[to_y][to_x] = selected_piece;
-                            self.tiles[from_y][from_x] = Tile::Empty;
-                        }
+                    if to_y - from_y == 1 {
+                        self.tiles[to_y][to_x] = selected_piece;
+                        self.tiles[from_y][from_x] = Tile::Empty;
+                    }
+                }
+            } else {
+                if from_y == 6 {
+                    if from_y - to_y == 1 || from_y - to_y == 2 {
+                        self.tiles[to_y][to_x] = selected_piece;
+                        self.tiles[from_y][from_x] = Tile::Empty;
+                    }
+                } else {
+                    if from_y - to_y == 1 {
+                        self.tiles[to_y][to_x] = selected_piece;
+                        self.tiles[from_y][from_x] = Tile::Empty;
                     }
                 }
             }
-        } else if let Tile::Empty = selected_piece {
-            unimplemented!("Fuck you");
+        } else if let Tile::Occupied(_, Piece::Knight) = selected_piece {
+            if (from_x as isize - to_x as isize).abs() == 1
+                && (from_y as isize - to_y as isize).abs() == 2
+            {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            } else if (from_x as isize - to_x as isize).abs() == 2
+                && (from_y as isize - to_y as isize).abs() == 1
+            {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            }
+        } else if let Tile::Occupied(_, Piece::Bishop) = selected_piece {
+            if (from_x as isize - to_x as isize).abs() == (from_y as isize - to_y as isize).abs() {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            }
+        } else if let Tile::Occupied(_, Piece::Rook) = selected_piece {
+            if (from_x as isize - to_x as isize) != 0 && (from_y as isize - to_y as isize) == 0 {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            } else if (from_x as isize - to_x as isize) == 0
+                && (from_y as isize - to_y as isize) != 0
+            {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            }
+        } else if let Tile::Occupied(_, Piece::Queen) = selected_piece {
+            if (from_x as isize - to_x as isize) != 0 && (from_y as isize - to_y as isize) == 0 {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            } else if (from_x as isize - to_x as isize) == 0
+                && (from_y as isize - to_y as isize) != 0
+            {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            } else if (from_x as isize - to_x as isize).abs()
+                == (from_y as isize - to_y as isize).abs()
+            {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            }
+        } else if let Tile::Occupied(_, Piece::King) = selected_piece {
+            if (from_x as isize - to_x as isize).abs() <= 1
+                && (from_y as isize - to_y as isize).abs() <= 1
+            {
+                self.tiles[to_y][to_x] = selected_piece;
+                self.tiles[from_y][from_x] = Tile::Empty;
+            }
         } else {
             self.tiles[to_y][to_x] = selected_piece;
             self.tiles[from_y][from_x] = Tile::Empty;
@@ -89,5 +137,9 @@ mod tests {
         let (to_x, to_y) = input();
         let board = board.move_piece(from_x, from_y, to_x, to_y);
         Board::print_board(board);
+    }
+    #[test]
+    fn testing() {
+        println!("♔");
     }
 }
